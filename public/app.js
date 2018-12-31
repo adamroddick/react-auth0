e8f048ddfe89f5e44060e6d2527cefffb5b2d551
Adam Roddick <adam@Ads-2.local>
2018-12-31 18:22:27 +1100

    Update callback url for netlify

--- public/app.js
+++ public/app.js
@@ -46173,8 +46173,8 @@ exports.AUTH_CONFIG = void 0;
 var AUTH_CONFIG = {
   domain: 'adamroddick.au.auth0.com',
   clientId: '5HneCLQFG4CUDywZn5MZIWKb14Jdp2tg',
-  //callbackUrl: 'https://elastic-yonath-ae9854.netlify.com/callback'
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://elastic-yonath-ae9854.netlify.com/callback' //callbackUrl: 'http://localhost:3000/callback'
+
 };
 exports.AUTH_CONFIG = AUTH_CONFIG;
 
